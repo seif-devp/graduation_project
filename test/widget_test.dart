61db545aed87bd9e0da95a9256210b27807c4f91
ahmed <ahmedameh76@gmail.com>
2026-05-02 02:23:47 +0300

    text bta3t asm el main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:graduation_project/main.dart';
+import 'package:graduation_project/work.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Work());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
