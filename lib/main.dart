25dc0f7292292e2775a0401dc78b630872ccf214
ahmed <ahmedameh76@gmail.com>
2026-07-06 00:47:06 +0300

    add secure api key

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:graduation_project/core/services/app_initializer.dart';
 import 'package:graduation_project/work.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   
   // Initialize all app services
   await AppInitializer().initialize();
